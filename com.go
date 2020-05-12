cc976e5618248841dd93b7d9ba255a5095864d8a
crossliu <crossliu@kuainiugroup.com>
2020-05-12 11:33:09 +0800

    求数的阶乘

---
+++ com.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/fatih/color"
+		)
+
+func printer(s string) {
+	/**
+	rand.Seed(time.Now().UnixNano())
+	rdm := rand.Intn(60)
+	switch {
+	case rdm <= 10:
+		color.Cyan(s)
+	case rdm <= 20:
+		color.Red(s)
+	case rdm <= 30:
+		color.Yellow(s)
+	case rdm <= 40:
+		color.Green(s)
+	case rdm <= 50:
+		color.Blue(s)
+	case rdm <= 60:
+		color.HiRed(s)
+
+	} */
+
+	ch := make(chan int, 1)
+	select {
+	case ch <- 1:
+		color.Cyan(s)
+	case ch <- 1:
+		color.Red(s)
+	case ch <- 1:
+		color.Yellow(s)
+	case ch <- 1:
+		color.Green(s)
+	case ch <- 1:
+		color.Blue(s)
+	case ch <- 1:
+		color.HiRed(s)
+
+	}
+	<-ch
+	close(ch)
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 // 第一个golang程序
+// go run main.go com.go
 func main() {
-	fmt.Println("hello golang")
+	printer("hello golang")
+	mutirun(6)
+	for i:=7; i<=14; i++ {
+		mutirun(i)
+	}
+
+	printer("------------end---------------")
+}
+//顺序编程: 求一个数的阶乘
+func mutirun(n int) {
+	 str, total := "", 1
+	for i:=1;i<=n;i++ {
+		str += " "+strconv.Itoa(i)+" x"
+		total *= i
+	}
+	res := fmt.Sprintf("求%02d的阶乘结果：%02d! = %s = %d", n, n, str[:len(str)-2], total)
+	printer(res)
 }
