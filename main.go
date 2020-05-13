56c1d21e1cbee826518cb8c1cbcebf485c4d0426
crossliu <crossliu@kuainiugroup.com>
2020-05-13 08:41:09 +0800

    阶乘算法，冒泡排序，九九乘法口诀

--- main.go
+++ main.go
@@ -8,20 +8,48 @@ import (
 // go run main.go com.go
 func main() {
 	printer("hello golang")
-	mutirun(6)
+	Mutirun(6)
 	for i:=7; i<=14; i++ {
-		mutirun(i)
+		Mutirun(i)
 	}
-
+	//---
+	bubble := []int{5,1,9,7,2,4,2,0,6,9,3}
+	BubbleSort(bubble)
+	printer(fmt.Sprint(bubble))
+	Mutiply()
 	printer("------------end---------------")
 }
 //顺序编程: 求一个数的阶乘
-func mutirun(n int) {
+func Mutirun(n int) {
 	 str, total := "", 1
 	for i:=1;i<=n;i++ {
 		str += " "+strconv.Itoa(i)+" x"
 		total *= i
 	}
 	res := fmt.Sprintf("求%02d的阶乘结果：%02d! = %s = %d", n, n, str[:len(str)-2], total)
+	printer(res)
+}
+
+//冒泡算法
+func BubbleSort(arr []int) {
+	n := len(arr)
+	for i:=0;i<n;i++ {
+		for j:=i+1; j<n; j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+}
+
+//九九乘法表
+func Mutiply() {
+	 res := ""
+	for i:=1;i<=9;i++ {
+		for j:=1;j<=i;j++ {
+			res += fmt.Sprintf("%2d x %2d = %2d  ", j, i, i*j)
+		}
+		res += "\n"
+	}
 	printer(res)
 }
